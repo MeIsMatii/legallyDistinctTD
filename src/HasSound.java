3c1682b4d0b58e5780370eaab9535855d0efcc85
Vanquish1905 <vanquish.16.3.1905@gmail.com>
2026-04-28 10:21:21 +0200

    renamed Soundable to HasSound

---
+++ src/HasSound.java
@@ -0,0 +1,26 @@
+import greenfoot.*;
+
+
+public interface HasSound {
+
+    default void playSound(String soundFileName) {
+        GreenfootSound soundToPlay = new GreenfootSound(soundFileName);
+        soundToPlay.setVolume(SoundSettings.getInstance().getMasterVolume());//setzt lautstärke
+        soundToPlay.play();//spielt den sound
+    }
+
+    default GreenfootSound playSoundAndKeep(String soundFileName) {
+        //spielt einen sound der looped ()
+        GreenfootSound soundToControl = new GreenfootSound(soundFileName);
+        soundToControl.setVolume(SoundSettings.getInstance().getMasterVolume());//setzt lautstärke
+        return soundToControl;
+    }
+
+   //updated die lautstärke von einem existierendem sound
+    default void syncVolumeOf(GreenfootSound soundToSync) {
+        if (soundToSync != null) {
+            soundToSync.setVolume(SoundSettings.getInstance().getMasterVolume());
+        }
+    }
+    /// TODO make it automaticly ajust every volume --colin
+}

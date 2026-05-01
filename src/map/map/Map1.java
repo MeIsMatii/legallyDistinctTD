2bd28675aa775771143ee7d235ff424b20c93cce
matii <eichbauer.mathilo@gmail.com>
2026-05-01 17:09:08 +0200

    4 colin

--- src/map/map/Map1.java
+++ src/map/map/Map1.java
@@ -1,15 +1,18 @@
 package map.map;
 
+import entity.Player;
+import entity.tower.TestTower;
 import greenfoot.World;
-import map.util.ComingSoon;
 
 public class Map1 extends World {
 
     public Map1() {
         super(14, 9, 60);
         setBackground("map1placeholder.jpeg");
-        addObject(new ComingSoon(), 6, 6);
+        addObject(new TestTower(false, 2), 1, 1);
 
+
+        addObject(new TestTower(true, 4), 1,1);
     }
 
 

f1c74582df2af8f3a8de75427eaa6806820614e6
matii <eichbauer.mathilo@gmail.com>
2026-05-01 13:52:52 +0200

    Added simple path block for Julian

--- src/map/util/Path.java
+++ src/map/util/Path.java
@@ -2,9 +2,25 @@ package map.util;
 
 import entity.Entity;
 
-public class path extends Entity {
-    @Override
+public class Path extends Entity {
+    private final int nextPathX;
+    private final int nextPathY;
+
+
+    public Path(int nextPathX, int nextPathY) {
+        this.nextPathX = nextPathX;
+        this.nextPathY = nextPathY;
+    }
+
+    public int getNextPathX() {
+        return nextPathX;
+    }
+
+    public int getNextPathY() {
+        return nextPathY;
+    }
+
     public void onHit(Entity hitter) {
-        
+        return; ///nothing needs to be handled in here i think
     }
 }

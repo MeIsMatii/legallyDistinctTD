dab2789d87bf94d55897a6d8eb595d9f1adc0740
Colin <vanquish.16.3.1905@gmail.com>
2026-05-01 11:04:23 +0200

    updated Volume slider so it supports dragging

--- src/setting/VolumeSlider.java
+++ src/setting/VolumeSlider.java
@@ -1,6 +1,5 @@
 package setting;
 
-
 import greenfoot.Color;
 import greenfoot.Greenfoot;
 import greenfoot.GreenfootImage;
@@ -10,10 +9,11 @@ import util.MainClass;
 
 public class VolumeSlider extends MainClass implements Clickable {
 
-
-    private static final int VolumeStepsPerClick = 10;
     private static final int SliderWidthPixel = 200;
     private static final int SliderHightPixel = 30;
+
+    // Tracks whether the user started their drag on the slider specifically.
+    private boolean isDraggingSlider = false;
 
     public VolumeSlider() {
         redrawSlider();
@@ -21,8 +21,23 @@ public class VolumeSlider extends MainClass implements Clickable {
 
     @Override
     public void act() {
+        MouseInfo mouseDetails = Greenfoot.getMouseInfo();
+        if (mouseDetails != null) {
+            //zählt nur als drag wenn die maus über dme slider is
+            if (mouseDetails.getButton() == 1 && isMouseOverSlider(mouseDetails)) {
+                isDraggingSlider = true;
+            }
+            //stoppt drag
+            if (mouseDetails.getButton() == 0) {
+                isDraggingSlider = false;
+            }
+            if (isDraggingSlider) {
+                updateVolumeFromMousePosition(mouseDetails);
+            }
+        }
+
         checkClick();
-        redrawSlider();//zum updaten des sliders
+        redrawSlider();
     }
 
     @Override
@@ -31,25 +46,34 @@ public class VolumeSlider extends MainClass implements Clickable {
         if (mouseDetails == null) {
             return;
         }
-        //pixelgröße X zu ZellSize
+        updateVolumeFromMousePosition(mouseDetails);
+    }
+
+    // returns ob die maus im slider ist
+    private boolean isMouseOverSlider(MouseInfo mouseDetails) {
+        int pixelsPerCell = getWorld().getCellSize();
+        int actorCenterX = getX() * pixelsPerCell + pixelsPerCell / 2;
+        int actorCenterY = getY() * pixelsPerCell + pixelsPerCell / 2;
+        int mouseX = mouseDetails.getX() * pixelsPerCell + pixelsPerCell / 2;
+        int mouseY = mouseDetails.getY() * pixelsPerCell + pixelsPerCell / 2;
+        return Math.abs(mouseX - actorCenterX) <= SliderWidthPixel / 2 && Math.abs(mouseY - actorCenterY) <= SliderHightPixel / 2;
+    }
+
+    private void updateVolumeFromMousePosition(MouseInfo mouseDetails) {
         int pixelsPerCell = getWorld().getCellSize();
         int actorLeftEdgeInPixels = getX() * pixelsPerCell - SliderWidthPixel / 2;
-
-        // Wo hat der spieler hingedrückt
         int mouseInPixelsX = mouseDetails.getX() * pixelsPerCell;
-        double clickPositionAlongBar = (double) (mouseInPixelsX - actorLeftEdgeInPixels) / SliderWidthPixel;
 
-        // lauter oder leiser machen
-        if (clickPositionAlongBar < 0.5) {
-            SoundSettings.getInstance().decreaseVolume(VolumeStepsPerClick);
-        } else {
-            SoundSettings.getInstance().increaseVolume(VolumeStepsPerClick);
-        }
+        // Mausposition zu prozent
+        double fractionAlongSlider = (double) (mouseInPixelsX - actorLeftEdgeInPixels) / SliderWidthPixel;
+        int newVolume = (int) (fractionAlongSlider * 100);
+        SoundSettings.getInstance().setMasterVolume(newVolume);
+        SoundSettings.getInstance().syncGlobalVolume();
     }
 
     private void redrawSlider() {
         int currentVolume = SoundSettings.getInstance().getMasterVolume();
-        int filledWidthInPixels = (int) ((currentVolume / 100.0) * SliderWidthPixel);//wie viel soll ausgefüllt sein
+        int filledWidthInPixels = (int) ((currentVolume / 100.0) * SliderWidthPixel);
         GreenfootImage sliderImage = new GreenfootImage(SliderWidthPixel + 2, SliderHightPixel + 20);
 
         // Leerenbereich als hintergrund erstellen
@@ -64,12 +88,18 @@ public class VolumeSlider extends MainClass implements Clickable {
         sliderImage.setColor(Color.BLACK);
         sliderImage.drawRect(0, 0, SliderWidthPixel, SliderHightPixel);
 
+        // schwarze linie um zu zeigen wo die volume ist
+        sliderImage.setColor(Color.BLACK);
+        sliderImage.fillRect(filledWidthInPixels - 2, 0, 4, SliderHightPixel);
+
+        // wert in weiß reinschreiben
+        sliderImage.setColor(Color.WHITE);
+        sliderImage.drawString(currentVolume + "%", Math.max(2, filledWidthInPixels / 2 - 10), SliderHightPixel - 8);
+
         //nummer wert drunter schreiben
         sliderImage.setColor(Color.BLACK);
         sliderImage.drawString("Volume: " + currentVolume + "%", 55, SliderHightPixel + 15);
 
         setImage(sliderImage);
-
-        ///TODO make slider jump to the mouse instead of incrementing by certain steps --mathilo 4 colin
     }
 }

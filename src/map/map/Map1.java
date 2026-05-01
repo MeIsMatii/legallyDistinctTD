8bcf5b75329a488a5a2c443ab2f71f0ab7a2b7c2
sophia <sophia.los2020@gmail.com>
2026-05-01 20:19:03 +0200

    MAde Map1 the Map(format needs to be fitted)

--- src/map/map/Map1.java
+++ src/map/map/Map1.java
@@ -7,8 +7,8 @@ import util.HomeButton;
 public class Map1 extends World {
 
     public Map1() {
-        super(14, 9, 60);
-        setBackground("map1placeholder.jpeg");
+        super(70, 45, 60);
+        setBackground("Map1.jpg");
         addObject(new HomeButton(),1,1);
         addObject(new Player(),6,6);
     }

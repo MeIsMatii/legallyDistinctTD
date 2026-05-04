704271d371cae8065a3b7d1ad075e97e595cbc08
jbeecom <janniblech@gmail.com>
2026-05-04 12:59:50 +0200

    gemacht das health und coins anzeige immer geupdatet werden und ein coinChester für testen dann später

--- src/hud/Player.java
+++ src/hud/Player.java
@@ -1,4 +1,5 @@
 package hud;
+import greenfoot.Greenfoot;
 import greenfoot.World;
 import util.ImageDisplay;
 import util.MainClass;
@@ -34,13 +35,23 @@ public class Player extends MainClass {
         health = startHealth;
     }                                               //noch sehr stolz auf mich
 
+    public void act() {
+        coinCheat();
+        show(getWorld());
+    }
 
-    public void addedToWorld(World world){                               //zeigt Leben oben auf der map
+    public void show(World world){                               //zeigt Leben oben auf der map
         world.addObject(new ImageDisplay("Credits.png", 300, 60),1,1);  //ganz oben Links, jemand muss noch herz bild ertsellen und dann hier einfügen
         world.showText("Leben: " + getHealth(), 2, 1);//ganz oben links,rechts vom herzen
 
         world.addObject(new ImageDisplay("Credits.png", 300, 60),1,2);  //ganz oben Links aber unter dem herzen, jemand muss noch coins bild ertsellen und dann hier einfügen
         world.showText("Coins: " + getCoins(), 2, 2);//rechts vom coinpng
+    }
+
+    public void coinCheat(){
+        if (Greenfoot.isKeyDown("c")){
+            setCoins(getCoins() + 100000);
+        }
     }
 
 }

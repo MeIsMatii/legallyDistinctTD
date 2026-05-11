55f1523b75d7acc45cd3dfb7259b9b3df22b2b7e
julian <julian@erling.cc>
2026-05-11 13:07:44 +0200

    meow schau mal

--- src/map/util/Path.java
+++ src/map/util/Path.java
@@ -1,6 +1,9 @@
 package map.util;
 
 import entity.Entity;
+import entity.Hitbox;
+import greenfoot.GreenfootImage;
+import greenfoot.World;
 
 public class Path extends Entity {
     private final int nextPathX;
@@ -10,6 +13,54 @@ public class Path extends Entity {
     public Path(int nextPathX, int nextPathY) {
         this.nextPathX = nextPathX;
         this.nextPathY = nextPathY;
+        setImage("cursor_invisible.png");
+        //Todo Real Path Size
+
+    }
+
+    public void addedToWorld(World world) {
+
+        int hitboxWidth = 0;
+        int hitboxHeight = 0;
+
+        int xOffset = 0;
+        int yOffset = 0;
+
+
+
+        if (getNextPathX() == 0 && getNextPathY() == 0) {
+            hitboxWidth = 80;
+            hitboxHeight = 80;
+
+
+        } else if (getX() == getNextPathX()) {
+            if (getY() < getNextPathY()) { //DOWN
+                hitboxHeight = getNextPathY() - getY();
+                yOffset=hitboxHeight;
+                //offset hH distance down
+
+            } else if (getY() > getNextPathY()) { //UP
+                hitboxHeight = getY() - getNextPathY();
+                yOffset=0;
+            }
+            hitboxWidth = 80;
+
+
+        } else if (getY() == getNextPathY()) {
+            if (getX() < getNextPathX()) { // RIGHT
+                hitboxWidth = getNextPathX() - getX();
+                xOffset=0;
+
+            } else if (getX() > getNextPathX()) { //LEFT
+                hitboxWidth = getX() - getNextPathX();
+                xOffset=hitboxWidth;
+            }
+            hitboxHeight = 80;
+        }
+        //System.out.printf("mewo: w: %d, h: %d\n", hitboxWidth, hitboxHeight);
+        System.out.println(xOffset);
+        Hitbox hitbox = new Hitbox(hitboxWidth, hitboxHeight, this);
+        getWorld().addObject(hitbox, getX()-xOffset, getY()-yOffset);
     }
 
     public int getNextPathX() {
@@ -27,4 +78,5 @@ public class Path extends Entity {
     public void onHover() {
         //nothing
     }
+
 }

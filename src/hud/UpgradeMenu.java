ed6d8d5d3a4a9bc73a30b181b7515e422be61585
Elias <eliasm.wolf@gmail.com>
2026-05-11 12:44:03 +0200

    More Bloons + clean up:)

---
+++ src/hud/UpgradeMenu.java
@@ -0,0 +1,11 @@
+package hud;
+
+import greenfoot.Actor;
+
+public class UpgradeMenu extends Actor {
+
+    public UpgradeMenu(){
+        setImage("commingSoon.png");
+        getImage().scale(1680,100);
+    }
+}

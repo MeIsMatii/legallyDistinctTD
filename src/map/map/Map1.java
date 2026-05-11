0b92d031bd0c19c303c1f26119e888591ce7267d
matii <eichbauer.mathilo@gmail.com>
2026-05-11 17:24:19 +0200

    scaled the map and made a fitting path for demonstration purposes

--- src/map/map/Map1.java
+++ src/map/map/Map1.java
@@ -1,6 +1,7 @@
 package map.map;
 
 import entity.tower.TestTower;
+import greenfoot.GreenfootImage;
 import hud.Player;
 import util.HomeButton;
 
@@ -9,13 +10,21 @@ public class Map1 extends Map {
 
 
     public Map1() {
-        setBackground("Map1.jpg");
+        //setBackground("Map1.jpg");
+
+        /// TODO BETTER MAP
+        GreenfootImage img = new GreenfootImage("Map1.jpg");
+
+        img.scale(1920,1080);
+        setBackground(img);
+
+
         addObject(new HomeButton(), 1, 1);
         addObject(new Player(), 6, 6);
 
-        addObject(new TestTower(true, 200), 5, 20);
-        addObject(new TestTower(false, 200), 500, 200);
-        int[][] pathLocations = {{0, 200}, {300, 200}, {300, 600}, {700, 600}, {700, 300}, {1200, 300}, {1200, 800}, {500, 800}, {500, 500}, {900, 500}, {900, 1080}};
+        addObject(new TestTower(true, 400), 5, 20);
+        addObject(new TestTower(false, 200), 500, 300);
+        int[][] pathLocations = {{0,140},{800,140},{800,350},{1680,350},{1680,600},{470,600},{470,970},{1920,970}};
         super.addPath(pathLocations);
 
 

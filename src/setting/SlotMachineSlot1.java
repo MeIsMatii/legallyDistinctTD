d517285f42877617453631cf833ef9cf4c695739
Jannis <janniblech@gmail.com>
2026-05-14 18:15:04 +0200

    komplette katastrophe aber grundstruktur für gambeln

---
+++ src/setting/SlotMachineSlot1.java
@@ -0,0 +1,16 @@
+package setting;
+
+import greenfoot.Greenfoot;
+import greenfoot.World;
+import util.Clickable;
+import util.MainClass;
+
+public class SlotMachineSlot1 extends MainClass {
+
+    public SlotMachineSlot1() {
+        setImage("chinaFlaggeGamble.png");
+        getImage().scale(80, 80);
+        Gambling world = (Gambling)getWorld();
+    }
+
+}
---
+++ src/setting/SlotMachineSlot3.java
@@ -0,0 +1,16 @@
+package setting;
+
+import greenfoot.Greenfoot;
+import greenfoot.World;
+import util.Clickable;
+import util.MainClass;
+
+public class SlotMachineSlot3 extends MainClass {
+
+    public SlotMachineSlot3() {
+        setImage("lueckefueller2Gamble.jpg");
+        getImage().scale(80, 80);
+        Gambling world = (Gambling)getWorld();
+    }
+
+}

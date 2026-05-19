7e0d45a0c83647d51a849b8f889d5852d657b5f7
Febo <febo@kabelmail>
2026-05-19 10:36:30 +0200

    so Zeug mit TowerSelector action

--- src/ui/common/ImageDisplay.java
+++ src/ui/common/ImageDisplay.java
@@ -1,8 +1,9 @@
 package ui.common;
 
 import core.MainClass;
+import core.Clickable;
 
-public class ImageDisplay extends MainClass {
+public class ImageDisplay extends MainClass implements Clickable {
     public ImageDisplay(String image, int scaleX, int scaleY) {
         setImage(image);
         getImage().scale(scaleX, scaleY);

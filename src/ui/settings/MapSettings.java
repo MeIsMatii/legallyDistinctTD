fbade93894994d1d79c9048afd7078b7e8745bf9
duhicolin09 <vanquish.16..05.109@gmail.com>
2026-05-22 10:22:01 +0200

    Improved Settingspage

--- src/ui/settings/MapSettings.java
+++ src/ui/settings/MapSettings.java
@@ -3,8 +3,11 @@ package ui.settings;
 import core.HasSound;
 import greenfoot.Greenfoot;
 import greenfoot.GreenfootSound;
+import greenfoot.MouseInfo;
 import greenfoot.World;
 import ui.common.HomeButton;
+import ui.common.ImageDisplay;
+
 
 public class MapSettings extends World implements HasSound {
     private final GreenfootSound backgroundMusic;
@@ -12,14 +15,23 @@ public class MapSettings extends World implements HasSound {
     public MapSettings() {
         super(29, 29, 20);
         setBackground("dirtsquare.png");
-        addObject(new HomeButton(), 4, 4);
-        addObject(new VolumeSlider(), 6, 10);
+        addObject(new HomeButton(), 1, 1);
+        addObject(new VolumeSlider(), 9, 7);
         backgroundMusic = playSoundAndKeep("TEMPTropischSound.mp3");
-        addObject(new SongDropDown(), 20, 10);
+        addObject(new SongDropDown(),21,6);
+        addObject(new ImageDisplay("Lautsprecher.png", 30,30),2,7);
     }
 
     @Override
     public void act() {
+        debug();
+    }
+    private void debug(){
+        if (Greenfoot.isKeyDown("F3")){
+            MouseInfo mouseInfo = Greenfoot.getMouseInfo();
+            System.out.println("X: "+ mouseInfo.getX()+"    Y: "+ mouseInfo.getY() );
+            return;
+        }
         if (Greenfoot.isKeyDown("F2")) {
             backgroundMusic.playLoop();
         }

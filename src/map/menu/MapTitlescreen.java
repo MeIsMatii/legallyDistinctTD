85c78cfe26abceac8a87c52fc3c956d47b165083
matii <eichbauer.mathilo@gmail.com>
2026-05-29 23:44:54 +0200

    removed the player from a non level wolrd

--- src/map/menu/MapTitlescreen.java
+++ src/map/menu/MapTitlescreen.java
@@ -1,20 +1,16 @@
 package map.menu;
 
-import core.Player;
 import greenfoot.GreenfootImage;
 import greenfoot.World;
-import map.levels.Map;
-import ui.common.HomeButton;
-import ui.common.ImageDisplay;
 import ui.hud.StartingButton;
 
 public class MapTitlescreen extends World {
     public MapTitlescreen() {
-        super(1920,1080,1);
-        addObject(new Player(100,100),100,100);
+        super(1920, 1080, 1);
+        //addObject(new Player(100,100),100,100);
         GreenfootImage background = new GreenfootImage("StartingScreen.png");
         background.scale(1920, 1080);
         setBackground(background);
-        addObject(new StartingButton(),1000,800);
+        addObject(new StartingButton(), 1000, 800);
     }
 }

dc31ef876167071ed4199db4892f35071b16844e
The Righteous Hand Of The Father (Febo) <febo@kabelmail.de>
2026-05-29 10:06:28 +0200

    so Zeug mit TowerSelector action

--- src/ui/hud/TowerSelector.java
+++ src/ui/hud/TowerSelector.java
@@ -3,7 +3,6 @@ package ui.hud;
 import core.Clickable;
 import core.MainClass;
 import entities.tower.TestTower;
-import greenfoot.World;
 import ui.common.ImageDisplay;
 
 public class TowerSelector extends MainClass implements Clickable {
@@ -16,19 +15,17 @@ public class TowerSelector extends MainClass implements Clickable {
 
 
     }
-    @Override
+
     public void onClick() {
 
     }
 
     public void allTheUpgrades() {
-    }
-
-    @Override
-    protected void addedToWorld(World world) {
         ImageDisplay imageDisplay = new ImageDisplay("TestTower2_2.png",30,40);
         getWorld().addObject(imageDisplay,200,200);
+
     }
+
 
     @Override
     public void act() {

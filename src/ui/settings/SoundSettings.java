74ca73b836f0240aa03a79b6c98264456f2886c4
Colin <Vanquish.16.03.1805@gmail.com>
2026-05-30 17:45:43 +0200

    saves if the sound was muted and ability to mute and unmute while keeping the last set volume

--- src/ui/settings/SoundSettings.java
+++ src/ui/settings/SoundSettings.java
@@ -11,14 +11,23 @@ public class SoundSettings {
     private static final int MinimumVolume = 0;
     private static final int MaximumVolume = 100;
     private static final int DefaultStartingVolume = SaveManager.getInstance().getVolume();
+    private static boolean Muted = false;
 
     private static SoundSettings singleInstance = null;
 
     private final List<GreenfootSound> registeredSounds = new ArrayList<GreenfootSound>();
     private int masterVolume;
 
+    public static boolean isMuted() {
+        return Muted;
+    }
+
     private SoundSettings() {
-        this.masterVolume = DefaultStartingVolume;
+        if (SaveManager.getInstance().isSoundOn()){
+            this.masterVolume = DefaultStartingVolume;
+        }else {
+            muteALLSound();
+        }
     }
 
     public static SoundSettings getInstance() {
@@ -33,8 +42,27 @@ public class SoundSettings {
     }
 
     public void setMasterVolume(int newVolume) {
+        if (Muted){
+            unMuteAllSounds();
+            SaveManager.getInstance().setSoundOn(true);
+        }
         this.masterVolume = Math.max(MinimumVolume, Math.min(MaximumVolume, newVolume));
         SaveManager.getInstance().setVolume(this.masterVolume);
+    }
+
+
+    public void muteALLSound(){
+        this.masterVolume = MinimumVolume;
+        syncGlobalVolume();
+        Muted = true;
+        SaveManager.getInstance().setSoundOn(false);
+    }
+    public void unMuteAllSounds(){
+        SaveManager.getInstance().reload();
+        this.masterVolume = SaveManager.getInstance().getVolume();
+        syncGlobalVolume();
+        Muted = false;
+        SaveManager.getInstance().setSoundOn(true);
     }
 
     public void addRegisteredSound(GreenfootSound soundToRegister) {

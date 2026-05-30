aeb3edc75f795ccc7e0c9fb0effa23b648f08565
Colin <Vanquish.16.03.1805@gmail.com>
2026-05-30 17:45:14 +0200

    new Mutebutton

---
+++ src/ui/settings/MuteButton.java
@@ -0,0 +1,41 @@
+package ui.settings;
+
+import core.Clickable;
+import core.MainClass;
+import greenfoot.GreenfootImage;
+import util.SaveManager;
+
+public class MuteButton extends MainClass implements Clickable {
+    private boolean muted = !SaveManager.getInstance().isSoundOn();
+    public MuteButton(){
+        updateApperance();
+    }
+
+    @Override
+    public void act() {
+        checkClick();
+    }
+
+    @Override
+    public void onClick() {
+        if (SoundSettings.isMuted()){
+            SoundSettings.getInstance().unMuteAllSounds();
+            muted = false;
+        }else {
+            SoundSettings.getInstance().muteALLSound();
+            muted = true;
+        }
+        updateApperance();
+    }
+    public void updateApperance(){
+        if (muted){
+            GreenfootImage img = new GreenfootImage("LautsprecherMuted.png");
+            img.scale(30,30);
+            setImage(img);
+        }else {
+            GreenfootImage img = new GreenfootImage("Lautsprecher.png");
+            img.scale(30,30);
+            setImage(img);
+        }
+    }
+}

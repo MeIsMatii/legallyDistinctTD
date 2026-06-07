f882703575e567541440945e17cdb04f519ad79e
matii <eichbauer.mathilo@gmail.com>
2026-06-07 14:00:31 +0200

    removed tower from map

--- src/map/levels/Map1.java
+++ src/map/levels/Map1.java
@@ -23,8 +23,6 @@ public class Map1 extends Map {
         addObject(new HomeButton(), 1, 1);
         addObject(new Player(), 6, 6);
 
-        addObject(new TestTower(true, 400), 5, 20);
-        addObject(new TestTower(false, 200), 500, 300);
         int[][] pathLocations = {{0,140},{800,140},{800,350},{1680,350},{1680,600},{470,600},{470,970},{1920,970}};
         super.addPath(pathLocations);
 

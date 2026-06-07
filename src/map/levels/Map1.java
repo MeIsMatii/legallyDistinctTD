628ef42c7cb39751ef78de9982db10c6b29eed73
matii <eichbauer.mathilo@gmail.com>
2026-06-07 14:02:18 +0200

    removed unnecessary stuff

--- src/map/levels/Map1.java
+++ src/map/levels/Map1.java
@@ -20,8 +20,6 @@ public class Map1 extends Map {
 
         //addObject(new UpgradeMenu(),800, 1000); <- exact placment
 
-        addObject(new HomeButton(), 1, 1);
-        addObject(new Player(), 6, 6);
 
         int[][] pathLocations = {{0,140},{800,140},{800,350},{1680,350},{1680,600},{470,600},{470,970},{1920,970}};
         super.addPath(pathLocations);

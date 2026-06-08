537a9dd7a17970bb31f254c5c84fffec2779a640
julian <julian@erling.cc>
2026-06-08 12:45:51 +0200

    Pause Menu

--- src/map/levels/Map1.java
+++ src/map/levels/Map1.java
@@ -31,9 +31,9 @@ public class Map1 extends Map {
         //addObject(new RedBloon(2, 100), 1,138);
         //addObject(new MapCoordinatesUtilGuy(), 0,0);
         //addObject(new TestTower(, 400), 5, 20);
-        addObject(new TestTower(false, 2000), 500, 300);
+        addObject(new TestTower(false, 2000,4,5), 500, 300);
         int[][] pathLocations = {{1,138},{666,138},{666,355},{1407,355},{1407,614},{429,614},{429,991},{1620,991}};
-        int[][] pathLocations = {{0,140},{800,140},{800,350},{1680,350},{1680,600},{470,600},{470,970},{1920,970}};
+        //int[][] pathLocations = {{0,140},{800,140},{800,350},{1680,350},{1680,600},{470,600},{470,970},{1920,970}};
         super.addPath(pathLocations);
 
 

2b2e7b59339a0ae9700becc604abd4c7e1ce2698
Elias <eliasm.wolf@gmail.com>
2026-06-12 11:01:08 +0200

    Upgrades only have to add the display eventually

--- src/ui/hud/UpgradePath.java
+++ src/ui/hud/UpgradePath.java
@@ -26,67 +26,61 @@ public class UpgradePath extends Actor implements Clickable {
             return;
         }
         int maxUpgrade;
+        int maxPath = 5;
+
+
         switch (this.path) {
             case 1:
-                if(tower.getUpgrade2() > 0 && tower.getUpgrade3() > 0) {
-                    //locked
+                if (tower.getUpgrade2() > 0 && tower.getUpgrade3() > 0) {
+                    System.out.println("locked case 1");
                     break;
                 }
+
                 //max. 1 bought
                 maxUpgrade = Math.max(tower.getUpgrade2(), tower.getUpgrade3());
-
+                if (maxUpgrade >= 3) {
+                    maxPath = 2;
+                }
+                if (tower.getUpgrade1() < maxPath) {
+                    player1.setCoins(player1.getCoins() - 500);
+                    tower.upgrade1();
+                }
 
                 break;
             case 2:
-                if(tower.getUpgrade1() > 0 && tower.getUpgrade3() > 0) {
-                    //locked
+                if (tower.getUpgrade1() > 0 && tower.getUpgrade3() > 0) {
+                    System.out.println("locked case 2");
                     break;
                 }
                 maxUpgrade = Math.max(tower.getUpgrade1(), tower.getUpgrade3());
+                if (maxUpgrade >= 3) {
+                    maxPath = 2;
+                }
+                if (tower.getUpgrade2() < maxPath) {
+                    player1.setCoins(player1.getCoins() - 500);
+                    tower.upgrade2();
+                }
                 break;
             case 3:
-                if(tower.getUpgrade1() > 0 && tower.getUpgrade2() > 0) {
-                    //locked
+                if (tower.getUpgrade1() > 0 && tower.getUpgrade2() > 0) {
+                    System.out.println("locked case 3");
                     break;
                 }
                 maxUpgrade = Math.max(tower.getUpgrade1(), tower.getUpgrade2());
-
+                if (maxUpgrade >= 3) {
+                    maxPath = 2;
+                }
+                if (tower.getUpgrade3() < maxPath) {
+                    player1.setCoins(player1.getCoins() - 500);
+                    tower.upgrade3();
+                }
 
                 break;
             default:
                 System.out.println("upgrade path must be 0<x<=3");
                 break;
         }
-
-
-
-
-        if (path == 1) {
-            if (tower.getUpgrade2() <= 3 && tower.getUpgrade3() == 1 || tower.getUpgrade3() <= 3 && tower.getUpgrade2() == 1) {
-                if (tower.getUpgrade1() <= 5) {
-                    player1.setCoins(player1.getCoins() - 500);
-                    tower.upgrade1();
-                }
-            }
-        } else if (path == 2) {
-            if (tower.getUpgrade1() <= 3 && tower.getUpgrade3() == 1 || tower.getUpgrade3() <= 3 && tower.getUpgrade1() == 1) {
-                if (tower.getUpgrade2() <= 5) {
-                    player1.setCoins(player1.getCoins() - 500);
-                    tower.upgrade2();
-                }
-            }
-
-        } else if (path == 3) {
-            if (tower.getUpgrade2() <= 3 && tower.getUpgrade1() == 1 || tower.getUpgrade1() <= 3 && tower.getUpgrade2() == 1) {
-                if (tower.getUpgrade3() <= 5) {
-                    player1.setCoins(player1.getCoins() - 500);
-                    tower.upgrade3();
-                }
-            }
-        }
     }
-
-    @Override
     public void act() {
         checkClick();
     }

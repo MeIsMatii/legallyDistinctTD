196ca67c2969b50f6300a6c8be6723f128dbf0e4
matii <eichbauer.mathilo@gmail.com>
2026-06-16 21:21:43 +0200

    made it only placeable on path

--- src/entities/tower/TrapTower.java
+++ src/entities/tower/TrapTower.java
@@ -1,29 +1,22 @@
 package entities.tower;
 
 import core.Clickable;
-import entities.Entity;
+import entities.Hitbox;
 import entities.enemy.Enemy;
 import entities.projectiles.Mine;
-import entities.projectiles.TestProjectile;
-import entities.tower.util.RangeDisplay;
-import greenfoot.Color;
-import greenfoot.Greenfoot;
-import greenfoot.MouseInfo;
 import greenfoot.World;
-import map.levels.Map;
 import map.levels.util.Path;
-import ui.hud.UpgradeMenu;
 
 import java.util.List;
 
-public class TrapTower extends Tower implements Clickable{
+public class TrapTower extends Tower implements Clickable {
     private int mineRadius;
-    private int[] upgrades1 = new int[]{150,500,2500,7500,17000};
-    private int[] upgrades2 = new int[]{200,450,3000,10000,25000};
-    private int[] upgrades3 = new int[]{100,350,1750,6000, 9500};
+    private int[] upgrades1 = new int[]{150, 500, 2500, 7500, 17000};
+    private int[] upgrades2 = new int[]{200, 450, 3000, 10000, 25000};
+    private int[] upgrades3 = new int[]{100, 350, 1750, 6000, 9500};
 
     public TrapTower() {
-        super(125,true, 80, 99999999, 200, 10, 1, 90);
+        super(125, true, 80, 99999999, 200, 10, 1, 90);
         mineRadius = 150;
 
         setImage("TestTower2_2.png");
@@ -49,19 +42,19 @@ public class TrapTower extends Tower implements Clickable{
 
     @Override
     public String upgrade1() {
-        setUpgrade1(getUpgrade1()+1);
+        setUpgrade1(getUpgrade1() + 1);
         return "";
     }
 
     @Override
     public String upgrade2() {
-        setUpgrade2(getUpgrade2()+1);
+        setUpgrade2(getUpgrade2() + 1);
         return "";
     }
 
     @Override
     public String upgrade3() {
-        setUpgrade3(getUpgrade3()+1);
+        setUpgrade3(getUpgrade3() + 1);
         return "";
     }
 
@@ -69,22 +62,27 @@ public class TrapTower extends Tower implements Clickable{
     }
 
     public void act() {
-        if(isPaused()) return;
+        if (isPaused()) return;
         super.act();
     }
 
-    @Override
-    public void onHit(Entity hitter) {         //kann man jetzt überall platzieren
-        if (hitter instanceof Tower) {
-            setCanPlace(false);
-            return;
+
+    public void checkPlacement() {
+        List<Hitbox> hitboxes = getIntersectingObjects(Hitbox.class);
+        setCanPlace(false);
+        for (Hitbox hitbox : hitboxes) {
+            if (hitbox.getOWNER() instanceof Path) {
+                setCanPlace(true);
+                break;
+            }
         }
 
-        if (hitter instanceof Path) {
-            setCanPlace(true);
-            return;
+        if (getCanPlace()) {
+            getRANGEDISPLAY().setRangeVisibility(true, getColorGrey());
+        } else {
+            getRANGEDISPLAY().setRangeVisibility(true, getColorRed());
         }
-
-        setCanPlace(true);
     }
+
+
 }

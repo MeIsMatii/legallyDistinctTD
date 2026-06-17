0effb7ed82eadf6968e81f06b3ae0c4d2a030ec3
julian <julian@erling.cc>
2026-06-17 20:30:17 +0200

    GameOverPopUp

--- src/core/Player.java
+++ src/core/Player.java
@@ -65,7 +65,6 @@ public class Player extends MainClass {
     public void addedToWorld(World world) {
         world.addObject(new ImageDisplay("heart.png", 30, 30), 40, 40);  //ganz oben Links, jemand muss noch herz bild ertsellen und dann hier einfügen
         world.addObject(new ImageDisplay("Coin.png", 45, 45), 120, 40);  //ganz oben Links aber unter dem herzen, jemand muss noch coins bild ertsellen und dann hier einfügen
-        health = 0;
     }
 
 

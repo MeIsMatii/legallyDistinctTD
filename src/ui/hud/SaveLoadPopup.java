45d52b21d308946727b6e7ec2ae7314acea51694
Colin <vanquish.16.3.1905@gmail.com>
2026-06-21 22:18:25 +0200

    Added closePopupButton to Multiplayerpreview to close the Popup

--- src/ui/hud/SaveLoadPopup.java
+++ src/ui/hud/SaveLoadPopup.java
@@ -1,18 +1,31 @@
 package ui.hud;
+
 import core.MainClass;
 import greenfoot.*;
-public class SaveLoadPopup extends MainClass {
+import ui.hud.buttons.ClosePopupButton;
 
-    public SaveLoadPopup(String text){
+public class SaveLoadPopup extends MainClass {
+    private ClosePopupButton closeButton;
+
+    public SaveLoadPopup(String text) {
         int width = 800;
         int height = 600;
         GreenfootImage boxImage = new GreenfootImage(width, height);
-        boxImage.setColor(new Color(139, 69, 19));boxImage.fill();
+        boxImage.setColor(new Color(139, 69, 19));
+        boxImage.fill();
         boxImage.setColor(Color.WHITE);
         boxImage.setFont(new Font("Arial", true, false, 24));
         boxImage.drawString(text, 20, height / 2);
         setImage(boxImage);
 
+        closeButton = new ClosePopupButton(this);
     }
 
+    @Override
+    protected void addedToWorld(World world) {
+        // Automatically adds the close button to the top right of this popup when the popup is added
+        int buttonX = getX() + (getImage().getWidth() / 2) - 20;
+        int buttonY = getY() - (getImage().getHeight() / 2) + 20;
+        world.addObject(closeButton, buttonX, buttonY);
+    }
 }

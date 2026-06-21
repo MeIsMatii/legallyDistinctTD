5b74f816e4971633110894b7fe2bbb4394e0079d
Colin <vanquish.16.3.1905@gmail.com>
2026-06-21 17:44:16 +0200

    alternate Constructor to just use an image not a string

--- src/ui/common/ImageDisplay.java
+++ src/ui/common/ImageDisplay.java
@@ -1,11 +1,15 @@
 package ui.common;
 
 import core.MainClass;
+import greenfoot.GreenfootImage;
 import util.Clickable;
 
 public class ImageDisplay extends MainClass implements Clickable {
     public ImageDisplay(String image, int scaleX, int scaleY) {
         setImage(image);
         getImage().scale(scaleX, scaleY);
+    }
+    public ImageDisplay(GreenfootImage img){
+        setImage(img);
     }
 }

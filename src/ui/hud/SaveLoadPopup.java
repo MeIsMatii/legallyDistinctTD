6298b263971feb5bd056c8ab94c2919b4bf45af1
Matii <103849762+MeIsMatii@users.noreply.github.com>
2026-06-22 09:32:45 +0200

    Update SaveLoadPopup.java

--- src/ui/hud/SaveLoadPopup.java
+++ src/ui/hud/SaveLoadPopup.java
@@ -6,10 +6,12 @@ import ui.hud.buttons.ClosePopupButton;
 
 public class SaveLoadPopup extends MainClass {
     private ClosePopupButton closeButton;
+    private int worldNumber;
 
-    public SaveLoadPopup(String text) {
+    public SaveLoadPopup(String text, int worldNumber) {
         int width = 800;
         int height = 600;
+        this.worldNumber = worldNumber;
         GreenfootImage boxImage = new GreenfootImage(width, height);
         boxImage.setColor(new Color(139, 69, 19));
         boxImage.fill();
@@ -27,6 +29,9 @@ public class SaveLoadPopup extends MainClass {
         int buttonX = getX() + (getImage().getWidth() / 2) - 20;
         int buttonY = getY() - (getImage().getHeight() / 2) + 20;
         world.addObject(closeButton, buttonX, buttonY);
+
+        world.addObject(new LoadSaveButton(worldNumber),1200,700);
+        world.addObject(new NewSaveButton(worldNumber),800,700);
     }
 
     public void act() {

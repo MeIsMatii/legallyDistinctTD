4ada0df51d62663e0cc6ec588f8671c036b2465c
matii <eichbauer.mathilo@gmail.com>
2026-06-23 16:29:00 +0200

    made ImageDisplay not clickable

--- src/ui/common/ImageDisplay.java
+++ src/ui/common/ImageDisplay.java
@@ -4,7 +4,7 @@ import core.MainClass;
 import greenfoot.GreenfootImage;
 import util.Clickable;
 
-public class ImageDisplay extends MainClass implements Clickable {
+public class ImageDisplay extends MainClass{
     public ImageDisplay(String image, int scaleX, int scaleY) {
         setImage(image);
         getImage().scale(scaleX, scaleY);

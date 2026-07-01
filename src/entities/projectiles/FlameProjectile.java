e9cfc315300a067f493a2ebac63242b42f13f4b8
Colin <vanquish.16.3.1905@gmail.com>
2026-07-01 19:33:35 +0200

    fixed

--- src/entities/projectiles/FlameProjectile.java
+++ src/entities/projectiles/FlameProjectile.java
@@ -1,11 +1,14 @@
 package entities.projectiles;
 
 import entities.tower.Tower;
+import greenfoot.GreenfootImage;
 
 public class FlameProjectile extends Projectile {
     public FlameProjectile(Tower owner) {
         super(owner);
-        setImage("flame.png");
+        GreenfootImage img = new GreenfootImage("flame.png");
+        img.scale(100,150);
+        setImage(img);
     }
 
     void move() {

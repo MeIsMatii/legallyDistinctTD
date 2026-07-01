e9d145eba57757119c52b45e0f80f6d89b4a04db
Colin <vanquish.16.3.1905@gmail.com>
2026-07-01 19:00:46 +0200

    implemented projectile.png.png

--- src/entities/projectiles/TestProjectile.java
+++ src/entities/projectiles/TestProjectile.java
@@ -1,11 +1,14 @@
 package entities.projectiles;
 
 import entities.tower.Tower;
+import greenfoot.GreenfootImage;
 
 public class TestProjectile extends Projectile {
     public TestProjectile(Tower owner) {
         super(owner);
-        setImage("heart.png");
+        GreenfootImage img = new GreenfootImage("projectile.png");
+        img.scale(100,60);
+        setImage(img);
     }
 
 

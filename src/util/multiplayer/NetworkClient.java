c7c7df2116d374944df9964c1a0f3a286c63efeb
matii <eichbauer.mathilo@gmail.com>
2026-07-14 23:18:26 +0200

    maploading may work now

--- src/util/multiplayer/NetworkClient.java
+++ src/util/multiplayer/NetworkClient.java
@@ -1,5 +1,10 @@
 package util.multiplayer;
 
+import greenfoot.Greenfoot;
+import map.levels.*;
+import map.menu.LoadingScreen;
+import util.saves.SaveManager;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,7 +13,7 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 
-public class NetworkClient implements MultiplayerConnection{
+public class NetworkClient implements MultiplayerConnection {
     private final String hostIp;
     private final int port;
     private PrintWriter out;
@@ -33,11 +38,13 @@ public class NetworkClient implements MultiplayerConnection{
             System.out.println("Client: Successfully connected to host!");
 
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            while (true)  { //endlosschleife wohoo --Mathilo
+            while (true) { //endlosschleife wohoo --Mathilo
                 String msg = in.readLine();
-                if(msg != null) {
+                if (msg != null && !msg.startsWith("MAP:")) {
                     NetworkManager.getInstance().queueIncomingMessage(msg);
                     System.out.println("Incoming message: " + msg);
+                } else if (msg != null && msg.startsWith("MAP:")) {
+                    setMap(Integer.parseInt(msg.substring(4)));
                 }
             }
         } catch (SocketTimeoutException e) {
@@ -46,5 +53,47 @@ public class NetworkClient implements MultiplayerConnection{
         } catch (IOException e) {
             System.err.println("Client Connection Error: " + e.getMessage());
         }
+    }
+
+    public void setMap(int mapNr) {
+        Map nextWorld = null;
+        switch (mapNr) {
+            case 1:
+                nextWorld = new Map1();
+                break;
+            case 2:
+                nextWorld = new Map2();
+                break;
+            case 3:
+                nextWorld = new Map3();
+                break;
+            case 4:
+                nextWorld = new Map4();
+                break;
+            case 5:
+                nextWorld = new Map5();
+                break;
+            case 6:
+                nextWorld = new Map6();
+                break;
+            case 7:
+                nextWorld = new Map7();
+                break;
+            case 8:
+                nextWorld = new Map8();
+                break;
+            case 9:
+                nextWorld = new Map9();
+                break;
+
+            default:
+                throw new RuntimeException("recieved map is invalid");
+        }
+        LoadingScreen ls = new LoadingScreen();
+        Greenfoot.setWorld(ls);
+        ls.setNextWorld(nextWorld);
+
+        SaveManager.getInstance().setLastMap(mapNr);
+        nextWorld.getGameSaveManager().saveGame();
     }
 }

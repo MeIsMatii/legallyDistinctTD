2518ca6e8ddccf63666029e34816b16f8bcf018a
mati <eichbauer.mathilo@gmail.com>
2026-07-24 11:07:55 +0200

    temp paths

--- src/maps/levels/GameMap4.java
+++ src/maps/levels/GameMap4.java
@@ -1,17 +1,22 @@
 package maps.levels;
 
 import core.Player;
+import greenfoot.GreenfootImage;
+import maps.levels.util.MapCoordinatesUtilGuy;
 import maps.menu.MapSelector;
 import ui.common.BackButton;
 import ui.common.ImageDisplay;
 
 public class GameMap4 extends GameMap {
     public GameMap4() {
-        setBackground("cell.jpg");
-        addObject(new ImageDisplay("comingSoon.png", 120, 100), 6, 6);
-        addObject(new Player(), 6, 6);
-        addObject(new BackButton(new MapSelector()), 0, 0);
+        GreenfootImage img = new GreenfootImage("Map4.png");
 
+        img.scale(1620, 1080);
+        setBackground(img);
+
+        addObject(new MapCoordinatesUtilGuy(),0,0);
+        int[][] pathLocations = {{0,233},{752,233},{752,531},{1414,531},{1414,984}};
+        super.addPath(pathLocations);
     }
 
     @Override

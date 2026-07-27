85fa2ddf684f76fb6442369da695abca4928301a
mati <eichbauer.mathilo@gmail.com>
2026-07-27 13:45:17 +0200

    mapguy

--- src/maps/levels/GameMap1.java
+++ src/maps/levels/GameMap1.java
@@ -13,7 +13,6 @@ public class GameMap1 extends GameMap {
     public GameMap1(boolean isMultiplayer, boolean isHost) {
         super(isMultiplayer, isHost);
 
-        addObject(new MapCoordinatesUtilGuy(), 0, 0);
         int[][] pathLocations = {{0, 233}, {752, 233}, {752, 531}, {1414, 531}, {1414, getHeight()}};
         super.addPath(pathLocations);
     }

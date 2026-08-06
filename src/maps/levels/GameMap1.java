9902338a458ad5e28eb8695947bd52634c75830f
Colin <vanquish.16.3.1905@gmail.com>
2026-08-06 13:46:16 +0200

    removed comment

--- src/maps/levels/GameMap1.java
+++ src/maps/levels/GameMap1.java
@@ -15,7 +15,6 @@ public class GameMap1 extends GameMap {
     public GameMap1() {
         setPaintOrder(RetryButton.class, MuteButton.class, SongButton.class, WaveResetButton.class,CloseButton.class, SongDropDown.class, VolumeSlider.class,SettingsPopup.class, SettingsButton.class,BackButton.class, PlayOnButton.class,PauseMenu.class);
 
-        /// TODO BETTER MAP
         GreenfootImage img = new GreenfootImage("Maps/Map1.png");
 
         img.scale(1620, 1080);

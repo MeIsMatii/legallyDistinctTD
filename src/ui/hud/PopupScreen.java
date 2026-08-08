58fde0c8d6025b2ffaf7cb162735fa94345ff7d3
matii <eichbauer.mathilo@gmail.com>
2026-08-08 16:41:44 +0200

    unified popup image

--- src/ui/hud/PopupScreen.java
+++ src/ui/hud/PopupScreen.java
@@ -2,7 +2,19 @@ package ui.hud;
 
 import core.MainClass;
 import greenfoot.Actor;
+import greenfoot.Color;
+import greenfoot.GreenfootImage;
 
+/**
+ * @author Jan
+ * @author Mathilo
+ */
 public abstract class PopupScreen extends Actor {
+    public PopupScreen() {
+        GreenfootImage img = new GreenfootImage(1500, 700);
+        img.setColor(Color.GRAY);
+        img.fill();
+        setImage(img);
+    }
     public abstract void onRemove();
 }

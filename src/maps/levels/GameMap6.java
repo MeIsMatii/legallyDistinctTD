20b372caf6ac62941ca1a35f1025940f9b1181b4
Colin <vanquish.16.3.1905@gmail.com>
2026-08-08 11:49:11 +0200

    made paths for every map

--- src/maps/levels/GameMap6.java
+++ src/maps/levels/GameMap6.java
@@ -4,16 +4,14 @@ import maps.levels.util.MapCoordinatesUtilGuy;
 
 public class GameMap6 extends GameMap {
     public GameMap6() {
-        addObject(new MapCoordinatesUtilGuy(), 0, 0);
-        int[][] pathLocations = {{0, 233}, {752, 233}, {752, 531}, {1414, 531}, {1414, 984}};
+        int[][] pathLocations = {{192,6},{192,809},{1613,809}};
         super.addPath(pathLocations);
 
     }
 
     public GameMap6(boolean isMultiplayer, boolean isHost) {
         super(isMultiplayer, isHost);
-        addObject(new MapCoordinatesUtilGuy(), 0, 0);
-        int[][] pathLocations = {{0, 233}, {752, 233}, {752, 531}, {1414, 531}, {1414, 984}};
+        int[][] pathLocations = {{192,6},{192,809},{1613,809}};
         super.addPath(pathLocations);
 
     }

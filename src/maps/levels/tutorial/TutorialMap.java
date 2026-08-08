0b25334b05a85b3d2246cd16fc2efce9ac270741
matii <eichbauer.mathilo@gmail.com>
2026-08-08 12:26:48 +0200

    bugfix

--- src/maps/levels/tutorial/TutorialMap.java
+++ src/maps/levels/tutorial/TutorialMap.java
@@ -6,11 +6,6 @@ import maps.levels.GameMap;
 public class TutorialMap extends GameMap {
 
     public TutorialMap(){
-        GreenfootImage img = new GreenfootImage("Maps/Map1.png");
-
-        img.scale(1620, 1080);
-        setBackground(img);
-
         int[][] pathLocations = {{0,233},{752,233},{752,531},{1414,531},{1414,getHeight()}};
         super.addPath(pathLocations);
         setWave(0);
@@ -18,7 +13,7 @@ public class TutorialMap extends GameMap {
 
     @Override
     public int getMapNumber() {
-        return 0;
+        return 1;
     }
 
 }

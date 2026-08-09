3042c19351dba860eb49fe73d53793415260d82b
Colin <vanquish.16.3.1905@gmail.com>
2026-08-09 13:03:31 +0200

    added author

--- src/ui/common/ClickawayImageDisplay.java
+++ src/ui/common/ClickawayImageDisplay.java
@@ -3,6 +3,9 @@ package ui.common;
 import greenfoot.Greenfoot;
 import util.Clickable;
 
+/**
+ * @Author Colin
+ */
 public class ClickawayImageDisplay extends ImageDisplay implements Clickable {
 
     public ClickawayImageDisplay(String image, int scaleX, int scaleY) {

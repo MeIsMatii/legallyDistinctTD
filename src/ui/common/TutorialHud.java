27e545145293e10f8438c30ba0b674be8458d29a
Colin <vanquish.16.3.1905@gmail.com>
2026-08-10 17:30:38 +0200

    added author

--- src/ui/common/TutorialHud.java
+++ src/ui/common/TutorialHud.java
@@ -5,6 +5,9 @@ import maps.levels.tutorial.TutorialMap;
 import ui.hud.buttons.Button;
 import util.Clickable;
 
+/**
+ * @author sophia
+ */
 public class TutorialHud extends Button implements Clickable {
     public TutorialHud() {
         setImage("buttons/StartingButton.PNG");

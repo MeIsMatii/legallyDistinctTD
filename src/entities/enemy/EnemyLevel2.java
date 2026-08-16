2dde09ae38089c2504404cab201d37d23ba91392
Colin <Vanquish.16.03.1805@gmail.com>
2026-08-16 16:47:44 +0200

    fixed images for enmies

--- src/entities/enemy/EnemyLevel2.java
+++ src/entities/enemy/EnemyLevel2.java
@@ -6,7 +6,7 @@ package entities.enemy;
 public class EnemyLevel2 extends Enemy {
     public EnemyLevel2() {
         super(1.25, 1);
-        setImage("enemies/arealEnemy1.png");
+        setImage("enemies/sicel.png");
     }
 
     @Override

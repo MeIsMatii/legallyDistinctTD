48f6f7f0589df3aa4fd040b988af96425fb5c10f
Colin <Vanquish.16.03.1805@gmail.com>
2026-08-16 13:04:12 +0200

    Fiexed nullpointer issue

--- src/ui/hud/buttons/NewSaveButton.java
+++ src/ui/hud/buttons/NewSaveButton.java
@@ -78,7 +78,9 @@ public class NewSaveButton extends Button {
 
         DifficultySelectorPopup difficultySelectorPopup = new DifficultySelectorPopup(nextWorld);
         getWorld().addObject(difficultySelectorPopup, getWorld().getWidth() / 2, getWorld().getHeight() / 2);
+        if (owner != null){
+            owner.onRemove();
+        }
 
-        owner.onRemove();
     }
 }

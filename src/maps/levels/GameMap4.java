9d4725ddb4c46e05d89f80321b0b4e7b9bb96830
Colin <Vanquish.16.03.1805@gmail.com>
2026-08-16 13:13:19 +0200

    fixed paths

--- src/maps/levels/GameMap4.java
+++ src/maps/levels/GameMap4.java
@@ -1,18 +1,20 @@
 package maps.levels;
+
+import maps.levels.util.MapCoordinatesUtilGuy;
+
 /**
  * @author Mathilo
  * @author colin
  */
 public class GameMap4 extends GameMap {
     public GameMap4() {
-
-        int[][] pathLocations = {{86, 1067}, {86, 434}, {1503, 434}, {1503, 5}};
+        int[][] pathLocations = {{1492,8},{1492,427},{84,427},{84,1017}};
         super.addPath(pathLocations);
     }
 
     public GameMap4(boolean isMultiplayer, boolean isHost) {
         super(isMultiplayer, isHost);
-        int[][] pathLocations = {{86, 1067}, {86, 434}, {1503, 434}, {1503, 5}};
+        int[][] pathLocations = {{1492,8},{1492,427},{84,427},{84,1017}};
         super.addPath(pathLocations);
     }
 
--- src/maps/levels/GameMap8.java
+++ src/maps/levels/GameMap8.java
@@ -8,15 +8,13 @@ import maps.levels.util.MapCoordinatesUtilGuy;
  */
 public class GameMap8 extends GameMap {
     public GameMap8() {
-        addObject(new MapCoordinatesUtilGuy(), 0, 0);
-        int[][] pathLocations = {{16, 163}, {1462, 163}, {1462, 694}, {205, 694}, {205, 1071}};
+        int[][] pathLocations = {{4,170},{1461,170},{1461,689},{207,689},{207,1017}};
         super.addPath(pathLocations);
-
     }
 
     public GameMap8(boolean isMultiplayer, boolean isHost) {
         super(isMultiplayer, isHost);
-        int[][] pathLocations = {{16, 163}, {1462, 163}, {1462, 694}, {205, 694}, {205, 1071}};
+        int[][] pathLocations = {{4,170},{1461,170},{1461,689},{207,689},{207,1017}};
         super.addPath(pathLocations);
 
     }

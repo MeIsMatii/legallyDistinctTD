5722102f43d5eb6bd13df9c4c23607e03eb9cac8
Colin <vanquish.16.3.1905@gmail.com>
2026-08-18 21:58:23 +0200

    fixed

--- src/entities/tower/Flamethrower.java
+++ src/entities/tower/Flamethrower.java
@@ -9,9 +9,10 @@ import greenfoot.World;
 
 public class Flamethrower extends Tower {
 
-
     private int magazine = 4;
+    private final int maxMagazine = 4;
     private int rechargeCounter;
+    private int rechargeDelay = 400; // Promoted to field so upgrades can alter it
 
     public Flamethrower() {
         super(350, true, 300, 1, 1, 10, 1, 45);
@@ -19,11 +20,12 @@ public class Flamethrower extends Tower {
         this.projectileSpawnOffset[1] = -10;
     }
 
+    @Override
     public void addedToWorld(World w) {
         super.addedToWorld(w);
         projectileToShoot = new FlameProjectile(this);
 
-        upgradeDescription3 = new String[]{"Faster shooting", "Even faster shooting", "The fastest Flamethrower"};
+        upgradeDescription3 = new String[]{"Faster reload", "Even faster reload", "The fastest Flamethrower"};
         upgradeDescription2 = new String[]{"Yellow hot", "White hot", "Blue hot"};
         upgradeDescription1 = new String[]{"More range", "Even more range", "Very long range"};
 
@@ -32,12 +34,10 @@ public class Flamethrower extends Tower {
         upgrade1Prices = new int[]{75, 1250, 10500};
     }
 
-
     @Override
     public void shoot(Enemy e) {
-        if(magazine <= 0) {
-            recharge();
-            return;
+        if (magazine <= 0) {
+            return; // Prevent shooting while empty; recharge() now runs in act()
         }
 
         magazine--;
@@ -55,73 +55,52 @@ public class Flamethrower extends Tower {
         return 0;
     }
 
-    private void recharge() { //works, no need to touch that ever again
-        int rechargeDelay = 400;
+    private void recharge() {
+        if (magazine >= maxMagazine) return; // Full ammo, no recharge needed
+
         if (rechargeCounter < rechargeDelay) {
             rechargeCounter++;
-            return;
+        } else {
+            magazine = maxMagazine;
+            rechargeCounter = 0;
         }
-        magazine = magazine + 4;
-        rechargeCounter = 0;
     }
 
+    @Override
     public void act() {
         if (isPaused()) return;
+        recharge(); // Continuously processes reload timer every tick
         super.act();
     }
 
+    @Override
     public void upgrade(int path) {
-        //TODO better upgrades
         switch (path) {
             case 1:
                 switch (getUpgrade1()) {
-                    case 1:
-                        setRange((getRange() * 1.1));
-                        break;
-                    case 2:
-                        setRange((getRange() * 1.3));
-                        //some kinda different behaviour
-                        break;
-                    case 3:
-                        setRange((getRange() * 1.5));
-                        //some kinda different behaviour
-                        break;
+                    case 1: setRange((int) (getRange() * 1.1)); break;
+                    case 2: setRange((int) (getRange() * 1.3)); break;
+                    case 3: setRange((int) (getRange() * 1.5)); break;
                 }
-
                 break;
 
             case 2:
                 switch (getUpgrade2()) {
-                    case 1:
-                        setProjectilePiercing(getProjectilePiercing() * 1.5);
-
-                        break;
-                    case 2:
-                        setProjectilePiercing(getProjectilePiercing() * 2);
-                        //some kinda different behaviour
-                        break;
-                    case 3:
-                        setProjectilePiercing(getProjectilePiercing() * 3);
-                        //some kinda different behaviour
-                        break;
+                    case 1: setProjectilePiercing((int) (getProjectilePiercing() * 1.5)); break;
+                    case 2: setProjectilePiercing(getProjectilePiercing() * 2); break;
+                    case 3: setProjectilePiercing(getProjectilePiercing() * 3); break;
                 }
                 break;
 
             case 3:
+                // Decreases downtime between magazines without breaking shot pacing
                 switch (getUpgrade3()) {
-                    case 1:
-                        setProjectileSpeed(getProjectileSpeed() * 1.5);
-                        break;
-                    case 2:
-                        setProjectileSpeed(getProjectileSpeed() * 2);
-                        //some kinda different behaviour
-                        break;
-                    case 3:
-                        setProjectileSpeed(getProjectileSpeed() * 3);
-                        //some kinda different behaviour
-                        break;
+                    case 1: rechargeDelay = 250; break;
+                    case 2: rechargeDelay = 120; break;
+                    case 3: rechargeDelay = 40;  break;
                 }
                 break;
+
             default:
                 System.out.println("Given Path must be between 1 & 3");
         }

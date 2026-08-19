481e4d29d7d8002e2a3efb9f962a3224e9650bbc
Vanquish1905 <c@c>
2026-08-19 12:39:46 +0200

    updataed enemies

--- src/entities/enemy/EnemyLevel6.java
+++ src/entities/enemy/EnemyLevel6.java
@@ -9,7 +9,7 @@ import entities.base.Enemy;
 public class EnemyLevel6 extends Enemy {
     public EnemyLevel6() {
         super(2.25, 200);
-        setImage("Coin.png"); //TODO FIX @elias/art guys
+        setImage("leviathan.png");
     }
 
     @Override

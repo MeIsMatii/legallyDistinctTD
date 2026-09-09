5608fa05c39a091d7c544697e69d9d3ab9f5e1d4
matii <eichbauer.mathilo@gmail.com>
2026-09-09 16:37:02 +0200

    better comments

--- docs/aufteilung.py
+++ docs/aufteilung.py
@@ -1,37 +1,26 @@
-def print_combined(values):
+def print_combined(values): #in percent, a valid suggestion should total 100
         total = 0
         for percent in values.values():
                 total += percent
         
         print("total : " + str(total))
 
-def calc_mss(mss_points, values):
+def calc_mss(mss_points, values): # in mss points. any value < 0 or > 15 is invalid and would redistribute the rest i think
         print("mss_points : " + str(mss_points))
         for name, percent in values.items():
                 print(name + " : " +  str(mss_points * len(values) * (percent/100)))
 
 
 mss_points = 11
-
-mathilo = 15
-colin = 14
-jannis = 13
-julian = 12
-febo = 12
-sophia = 12
-jan = 11
-elias = 11
-
-
-values = {
-        "mathilo" : mathilo,
-        "colin" : colin,
-        "jannis": jannis,
-        "julian" : julian,
-        "febo" : febo,
-        "sophia" : sophia,
-        "jan" : jan,
-        "elias": elias
+values = { #in percent
+        "mathilo" : 15,
+        "colin" : 14,
+        "jannis": 13,
+        "julian" : 12,
+        "febo" : 12,
+        "sophia" : 12,
+        "jan" : 11,
+        "elias": 11
         }
 
 
